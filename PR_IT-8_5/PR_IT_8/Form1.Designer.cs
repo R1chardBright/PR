5c1163576aa2776485ea8a6d7fe3b8ebbb3b326a
R1chardBright <70761721+R1chardBright@users.noreply.github.com>
2021-04-08 17:03:22 +0300

    PR8

---
+++ PR_IT-8_5/PR_IT_8/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Практика_8
+{
+  partial class pr
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // pr
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ControlLight;
+      this.ClientSize = new System.Drawing.Size(423, 317);
+      this.ForeColor = System.Drawing.Color.Black;
+      this.Name = "pr";
+      this.Text = "PR 8";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
